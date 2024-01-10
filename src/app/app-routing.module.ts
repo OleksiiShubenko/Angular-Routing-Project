c4c93e50b24f0089dcf8ee11989223575dfee845
Shubenko, Oleksii <oleksii.shubenko@kingfisher.com>
2024-01-10 18:35:46 +0200

    Task: 2 and 3 - Add pages and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {Route, RouterModule, Routes, UrlMatchResult, UrlSegment, UrlSegmentGroup} from '@angular/router';
+import { AboutComponent, AbcComponent, HomeComponent, PathNotFoundComponent } from './pages'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  {
+    component: AbcComponent,
+    matcher: (url: UrlSegment[], group: UrlSegmentGroup, route: Route): UrlMatchResult | null => {
+      console.log(url, group, route);
+      // один фрагмент, который включает 'abc'
+      return url.length === 1 && url[0].path.includes('abc') ? ({consumed: url}) : null;
+    }
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PathNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
