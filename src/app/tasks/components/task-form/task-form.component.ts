b754a7fb85a8ad6a0f66fc3d8fcd7074d6ca9b89
Shubenko, Oleksii <oleksii.shubenko@kingfisher.com>
2024-01-17 19:42:42 +0200

    Task: 12 - add bindingComponentInputs for taskId from pathParam to input in task-form component

--- src/app/tasks/components/task-form/task-form.component.ts
+++ src/app/tasks/components/task-form/task-form.component.ts
@@ -1,18 +1,26 @@
 import {Component, inject, type OnInit} from '@angular/core';
 import {TaskModel} from './../../models/task.model';
 import {TaskArrayService} from './../../services/task-array.service';
+import {Input} from "@angular/core";
 
 @Component({
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.css']
 })
 export class TaskFormComponent implements OnInit {
+  @Input({ alias: 'taskId'})
+  id!: string; // pathParam
+
   task!: TaskModel;
   private taskArrayService = inject(TaskArrayService);
 
   ngOnInit(): void {
     // create empty task to show at least empty form
     this.task = new TaskModel();
+
+    this.taskArrayService.getTask(this.id)
+      .then(task => this.task = task ?? {} as TaskModel)
+      .catch(err => console.log(err))
   }
 
   onSaveTask(): void {
