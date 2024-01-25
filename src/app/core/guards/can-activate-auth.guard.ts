0d5ff4c06a625e592bfb2dbef7de6b095cf9a198
Shubenko, Oleksii <oleksii.shubenko@kingfisher.com>
2024-01-25 19:54:43 +0200

    Task: 22 - Add auth service for canActivate guard

--- src/app/core/guards/can-activate-auth.guard.ts
+++ src/app/core/guards/can-activate-auth.guard.ts
@@ -1,6 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import {AuthService} from "../services/auth.service";
+import {inject} from "@angular/core";
 
 export const canActivateAuthGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const {url} = state;
+
   console.log('CanActivate Guard is called');
-  return true;
+
+  //return true or UrlTree by calling router.parseUrl('/login')
+  return authService.checkLogin(url)
+    ? true
+    : router.parseUrl('/login');
 };
--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,2 +1,3 @@
 export * from './guards/can-activate-auth.guard';
 export * from './services/messages.service'
+export * from './services/auth.service'
